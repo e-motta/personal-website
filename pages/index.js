f890931913c3ce7a8b3def6d277221b6c43ef527
Eduardo Motta de Moraes <edumoraes@live.com>
2023-01-05 15:17:41 -0300

    capitalize import names

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import FormattedDate from "../components/FormattedDate";
-import Layout, { siteTitle } from "../components/layout";
+import Layout, { siteTitle } from "../components/Layout";
 import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
 
